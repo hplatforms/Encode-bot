aa64a08b35f8ea8d9b8b45c7f89022baaa7b8a26
hplatforms <91740193+hplatforms@users.noreply.github.com>
2022-02-01 23:24:20 +0300

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -20,15 +20,15 @@ video_mimetype = [
 
 @app.on_message(filters.incoming & filters.command(['start', 'help']))
 def help_message(app, message):
-    message.reply_text(f"Hi {message.from_user.mention()}\nI can encode Telegram files in x265, just send me a video.", quote=True)
+    message.reply_text(f"Merhaba {message.from_user.mention()}\nTelegram dosyalarını x265'te kodlayabilirim, bana bir video göndermeniz yeterli.", quote=True)
 
 @app.on_message(filters.user(sudo_users) & filters.incoming & (filters.video | filters.document))
 def encode_video(app, message):
     if message.document:
       if not message.document.mime_type in video_mimetype:
-        message.reply_text("```Invalid Video !\nMake sure its a valid video file.```", quote=True)
+        message.reply_text("```Geçersiz Video!\nGeçerli bir video dosyası olduğundan emin olun.```", quote=True)
         return
-    message.reply_text("```Added to queue...```", quote=True)
+    message.reply_text("```Sıraya eklendi...```", quote=True)
     data.append(message)
     if len(data) == 1:
       add_task(message)
