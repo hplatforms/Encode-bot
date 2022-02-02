1fe9f9dac9acb941f3aba9a473a79d3213a98391
hplatforms <91740193+hplatforms@users.noreply.github.com>
2022-02-02 11:51:58 +0300

    Create translation.py

---
+++ bot/translation.py
@@ -0,0 +1,14 @@
+class Translation(object):
+       
+      DOWNLOAD_PROGRESS = "`█`"
+      UPLOAD_PROGRESS = "`░`"
+       
+      START_TEXT = """Merhaba {0}\nTelegram dosyalarını x265'te kodlayabilirim aynı zamanda HEVC olarak boyut düşürebilirim, bana bir video göndermeniz yeterli."""
+
+      PROGRESS = """`
+Yüzde : {0}%
+Tamamlanan: {1}
+Toplam: {2}
+Hız: {3}/s
+Süre: {4}
+`"""
